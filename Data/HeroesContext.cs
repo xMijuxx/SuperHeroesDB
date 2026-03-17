f0cf952413678a8e9ce102c5aed0791635f7dd6b
Julia Michniewicz <michniewiczjulka@gmail.com>
2026-03-17 18:29:23 +0100

    feat: add HeroesContext with Many to Many configuration

--- Data/HeroesContext.cs
+++ Data/HeroesContext.cs
@@ -5,6 +5,26 @@ namespace SuperHeroesDB.Data
 {
     public class HeroesContext : DbContext
     {
+        public HeroesContext(DbContextOptions<HeroesContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Power> Powers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<HeroPower> HeroPowers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroPower>()
+                .HasKey(hp => new { hp.HeroId, hp.PowerId });
+
+            modelBuilder.Entity<HeroPower>()
+                .HasOne(hp => hp.Hero)
+                .WithMany(h => h.HeroPowers)
+                .HasForeignKey(hp => hp.HeroId);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
