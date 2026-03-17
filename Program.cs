84194ae9a4bdb9cd7a3dc789027c1ffb83b5cde8
Julia Michniewicz <michniewiczjulka@gmail.com>
2026-03-17 18:40:56 +0100

    setup connection string in apsetting and register DbContext

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroesDB.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<HeroesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SuperHeroesDB")));
 
 var app = builder.Build();
 
